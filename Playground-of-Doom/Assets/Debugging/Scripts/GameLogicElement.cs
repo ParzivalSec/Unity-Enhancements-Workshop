6fc385cbe51f5f00496f2de99455ffb6ff75ee32
ParzivalSec <lukas.vogl12@gmail.com>
2017-05-11 00:34:00 +0200

    Created project, added Debugging_Scene, implemented on-screen FPSCounter, started implementing HTML-Logger

---
+++ Playground-of-Doom/Assets/Debugging/Scripts/GameLogicElement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogicElement : MonoBehaviour {
+
+    private HTMLLogger m_logger;
+
+	// Use this for initialization
+	void Start () {
+        m_logger = new HTMLLogger("log.html");
+
+        m_logger.Info(LogCategory.SYSTEM, "Out of Memory!");
+        m_logger.Info(LogCategory.NETWORKING, "DB connection lost");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Playground-of-Doom/Assets/Debugging/Scripts/HTMLLogger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+
+public enum LogCategory
+{
+    GAMEPLAY,
+    SYSTEM,
+    NETWORKING,
+    STATISTICS,
+}
+
+public class HTMLLogger : MonoBehaviour
+{
+    private string m_logFile = "";
+
+    public HTMLLogger(string logFile) {
+        m_logFile = Application.dataPath + "/" + logFile;
+        Debug.Log(m_logFile);
+        WriteHTMLHeader();
+    }
+
+    public void Info(LogCategory category, string msg) {
+        using (StreamWriter outputFile = new StreamWriter(m_logFile, true))
+        {
+            string logContainer = "<p class=" + category.ToString().ToLower() + "> " + msg + "</p>";
+
+            outputFile.Write(logContainer);
+        }
+    }
+
+    public void Warning(LogCategory category, string msg) {
+    }
+
+    public void Error(LogCategory category, string msg) {
+    }
+
+    private void WriteHTMLHeader() {
+        // Write the string array to a new file named "WriteLines.txt".
+        using (StreamWriter outputFile = new StreamWriter(m_logFile))
+        {
+            string htmlHeader =
+                "<html>" +
+                    "<head>" +
+                        "<script language='javascript' src='log.js'></script>" +
+                        "<link rel='stylesheet' href='log.css' />" +
+                    "</head>" +
+                    "<body>";
+
+            outputFile.Write(htmlHeader);
+        }
+    }
+}
